7c0eccbf806fc90392769df0266bb424b52d22f2
Will Mendes <willmendesneto@gmail.com>
2020-12-09 07:11:31 -0300

    fix: fixing test coverage lcov file

--- projects/ngx-copy-to-clipboard/karma.conf.js
+++ projects/ngx-copy-to-clipboard/karma.conf.js
@@ -16,10 +16,7 @@ module.exports = function (config) {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(
-        __dirname,
-        '../../coverage/ngx-skeleton-loader'
-      ),
+      dir: require('path').join(__dirname, '../../coverage/ngx-copy-to-clipboard'),
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true,
     },
